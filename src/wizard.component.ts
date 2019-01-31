05f9d1ec3f90331db6e4f638b4aec6a79f34c3fb
ghamry <ghamry648@gmail.com>
2019-01-31 11:45:37 +0300

    Update wizard.component.ts
    gh modifications

--- src/wizard.component.ts
+++ src/wizard.component.ts
@@ -5,22 +5,23 @@ import { WizardStepComponent } from './wizard-step.component';
   selector: 'form-wizard',
   template:
   `<div class="card">
-    <div class="card-header">
-      <ul class="nav nav-justified">
-        <li class="nav-item" *ngFor="let step of steps" [ngClass]="{'active': step.isActive, 'enabled': !step.isDisabled, 'disabled': step.isDisabled, 'completed': isCompleted}">
-          <a (click)="goToStep(step)">{{step.title}}</a>
-        </li>
-      </ul>
-    </div>
-    <div class="card-block">
-      <ng-content></ng-content>
-    </div>
-    <div class="card-footer" [hidden]="isCompleted">
-        <button type="button" class="btn btn-secondary float-left" (click)="previous()" [hidden]="!hasPrevStep || !activeStep.showPrev">Previous</button>
-        <button type="button" class="btn btn-secondary float-right" (click)="next()" [disabled]="!activeStep.isValid" [hidden]="!hasNextStep || !activeStep.showNext">Next</button>
-        <button type="button" class="btn btn-secondary float-right" (click)="complete()" [disabled]="!activeStep.isValid" [hidden]="hasNextStep">Done</button>
-    </div>
-  </div>`
+  <div class="card-header">
+    <ul class="nav nav-justified">
+      <li *ngFor="let step of steps" (click)="goToStep(step)" class="nav-item" [ngClass]="{'danger': !step?.isValid && !step.isDisabled ,
+      'active': step.isActive, 'enabled': !step.isDisabled, 'disabled': step.isDisabled, 'completed': isCompleted}">
+        <a>{{step.title}}</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-block">
+    <ng-content></ng-content>
+  </div>
+  <div class="card-footer" [hidden]="isCompleted">
+    <button type="button" class="btn btn-secondary float-left" (click)="previous()" [hidden]="!hasPrevStep || !activeStep.showPrev">Previous</button>
+    <button type="button" class="btn btn-secondary float-right" (click)="next()" [disabled]="!activeStep?.isValid" [hidden]="!hasNextStep || !activeStep.showNext">Next</button>
+    <button type="button" class="btn btn-secondary float-right" (click)="complete()" [disabled]="!activeStep?.isValid" [hidden]="hasNextStep">Done</button>
+  </div>
+</div>`
   ,
   styles: [
     '.card { height: 100%; }',
@@ -30,6 +31,7 @@ import { WizardStepComponent } from './wizard-step.component';
     '.nav-item { padding: 1rem 0rem; border-bottom: 0.5rem solid #ccc; }',
     '.active { font-weight: bold; color: black; border-bottom-color: #1976D2 !important; }',
     '.enabled { cursor: pointer; border-bottom-color: rgb(88, 162, 234); }',
+    '.danger { cursor: pointer; border-bottom-color: rgb(172, 22, 17)!important; }',
     '.disabled { color: #ccc; }',
     '.completed { cursor: default; }'
   ]
@@ -46,24 +48,49 @@ export class WizardComponent implements AfterContentInit {
 
   constructor() { }
 
+  
   ngAfterContentInit() {
+    // here assign the steps in the wizard to local steps
     this.wizardSteps.forEach(step => this._steps.push(step));
-    this.steps[0].isActive = true;
+
+    // if the user changes the steps at run time 
+    // this.wizardSteps.changes.subscribe((wizardStepsNew) => {
+    //   const tempSteps = [];
+    //   wizardStepsNew.forEach((newStep: any) => {
+    //     tempSteps.push(newStep);
+    //   });
+    //   this.steps = tempSteps; 
+    // });
+
+    // here assign the active step 
+    if (this.setActiveByIndex && this.setActiveByIndex > 0) {
+      this.steps[this.setActiveByIndex].isActive = true;
+
+      for (let index = 0; index < this.setActiveByIndex; index++) {
+        const el = this.steps[index];
+        el.isDisabled = false;
+      }
+    }
+    else
+      this.steps[0].isActive = true;
   }
 
-  get steps(): Array<WizardStepComponent> {
+  get steps(): Array<StepComponent> {
     return this._steps.filter(step => !step.hidden);
+  }
+  set steps(stp) {
+    this._steps = stp;
   }
 
   get isCompleted(): boolean {
     return this._isCompleted;
   }
 
-  get activeStep(): WizardStepComponent {
+  get activeStep(): StepComponent {
     return this.steps.find(step => step.isActive);
   }
 
-  set activeStep(step: WizardStepComponent) {
+  set activeStep(step: StepComponent) {
     if (step !== this.activeStep && !step.isDisabled) {
       this.activeStep.isActive = false;
       step.isActive = true;
@@ -83,16 +110,17 @@ export class WizardComponent implements AfterContentInit {
     return this.activeStepIndex > 0;
   }
 
-  public goToStep(step: WizardStepComponent): void {
-    if (!this.isCompleted) {
+  public goToStep(step: StepComponent): void {
+    if (!this.isCompleted && this.allStepsValid()) {
       this.activeStep = step;
     }
   }
 
   public next(): void {
     if (this.hasNextStep) {
-      let nextStep: WizardStepComponent = this.steps[this.activeStepIndex + 1];
+      let nextStep: StepComponent = this.steps[this.activeStepIndex + 1];
       this.activeStep.onNext.emit();
+
       nextStep.isDisabled = false;
       this.activeStep = nextStep;
     }
@@ -100,7 +128,7 @@ export class WizardComponent implements AfterContentInit {
 
   public previous(): void {
     if (this.hasPrevStep) {
-      let prevStep: WizardStepComponent = this.steps[this.activeStepIndex - 1];
+      let prevStep: StepComponent = this.steps[this.activeStepIndex - 1];
       this.activeStep.onPrev.emit();
       prevStep.isDisabled = false;
       this.activeStep = prevStep;
@@ -111,5 +139,7 @@ export class WizardComponent implements AfterContentInit {
     this.activeStep.onComplete.emit();
     this._isCompleted = true;
   }
+
+  public allStepsValid = (): boolean => this.steps.every(step => step.isValid);
 
 }
